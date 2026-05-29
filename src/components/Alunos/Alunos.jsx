15826644224c3820b3a09e8fceb8494dcfa220f7
Jenifer <jenifer.goncalves6@aluno.senai.br>
2026-05-29 08:56:17 -0300

    ADD: Testes na pasta de Alunos e App.jsx

---
+++ src/components/Alunos/Alunos.jsx
@@ -0,0 +1,13 @@
+import styles from './alunos.module.css'
+
+function Aluno(){
+    return (
+        <>
+        <p>nome: Jeni</p>
+        <p>idade: 17</p>
+        <p>ativo: Não</p>
+        </>
+    )
+}
+
+export default Aluno
